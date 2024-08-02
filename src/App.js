8ba44ccfb0a7580c4f99fd13707d81389ae31eba
Suleymanguly Malikov <suleymanmalikov03@gmail.com>
2024-08-02 04:18:42 +0200

    added some styles

--- src/App.js
+++ src/App.js
@@ -10,10 +10,22 @@ function range(start, end) {
 
 const App = () => {
   const [state, send] = useMachine(gameMachine);
+  // Result Message
+  const resultMessage = () => {
+    if (state.matches("winner")) {
+      const [winner, winningLines] = state.context.winner;
+      return `Player ${winner} wins!`;
+    }
+    if (state.matches("draw")) {
+      return "It's a draw!";
+    }
+    return `${state.context.player}'s turn`;
+  };
 
   return (
     <main>
       <h1>Tic Tac Toe</h1>
+      <p className="winningMessage">{resultMessage()}</p>
       <div className="game-board">
         {range(0, 9).map((index) => (
           <div
@@ -27,17 +39,14 @@ const App = () => {
           </div>
         ))}
       </div>
-      {/* <p>{state.context.winner}</p> */}
-      <div>
-        <pre>
-          {JSON.stringify(
-            { value: state.value, context: state.context },
-            null,
-            2
-          )}
-        </pre>
-        <button onClick={() => send({ type: "RESET" })}>Reset</button>
-      </div>
+      {/* {!state.matches("playing") && (
+        <button className="btn" onClick={() => send({ type: "RESET" })}>
+          Reset
+        </button>
+      )} */}
+      <button className="btn" onClick={() => send({ type: "RESET" })}>
+        Reset
+      </button>
     </main>
   );
 };
